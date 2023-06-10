f7ce6a32e2841457abc80fc6269b6971dce89011
nortigozab <77377179+nortigozab@users.noreply.github.com>
2023-06-10 12:29:19 -0500

    Ejercicio 3 'Sintaxis b3'

---
+++ Unidad_6/Ejercicio_3.js
@@ -0,0 +1,16 @@
+/**
+ *  * Ejemplo
+ */
+const PI = 3.1416;
+console.log(`El valor de PI es: ${PI}`);
+
+/**
+ *  * Ejercicio
+ */
+
+const seg_min = 60;
+const min_horas = 60;
+const horas_dia = 24;
+console.log(
+  `Un minuto tiene: ${seg_min} segundos, una hora tiene ${min_horas} minutos y un dia tiene ${horas_dia} horas`
+);
