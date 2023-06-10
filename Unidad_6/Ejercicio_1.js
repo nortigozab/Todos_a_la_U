96ecf0ee8b0c7d81133f918d1e97eccd53541fe1
nortigozab <77377179+nortigozab@users.noreply.github.com>
2023-06-10 12:13:35 -0500

    Ejercicio 1 'Sintaxis b1'

---
+++ Unidad_6/Ejercicio_1.js
@@ -0,0 +1,12 @@
+/*
+ * Crear una variable hola para imprimirla en un console.log
+ */
+var hola = "Hola mundo";
+console.log(hola);
+
+/**
+ * ? Crear una variable nombre y edad  para imprimirla en un console.log concatenamos con +
+ */
+var nombre = "Nicolas";
+var edad = 24;
+console.log(nombre + " " + edad);
