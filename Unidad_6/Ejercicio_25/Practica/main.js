4903eea4ed3f69beea59698f10068fc86322d38b
nortigozab <77377179+nortigozab@users.noreply.github.com>
2023-06-13 15:30:15 -0500

    Ejercicio 25 'API-REST'

---
+++ Unidad_6/Ejercicio_25/Practica/main.js
@@ -0,0 +1,23 @@
+const d = document;
+
+d.addEventListener("submit", (e) => {
+  e.preventDefault();
+  alert("enviando formulario");
+
+  const $response = d.querySelector(".contact-form-response");
+  $form = d.querySelector(".form");
+
+  fetch("https://formsubmit.co/ajax/nortigozab@correo.udistrital.edu.co", {
+    method: "POST",
+    body: new FormData(e.target),
+  })
+    .then((res) => (res.ok ? res.json() : Promise.reject(res)))
+    .then((json) => {
+      console.log(json);
+      $response.classList.remove("none");
+      $form.reset();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
