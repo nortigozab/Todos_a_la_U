c1f4bbdbe9a56dcc5c4ab6c52f63dd9210de3bba
nortigozab <77377179+nortigozab@users.noreply.github.com>
2023-06-23 16:22:17 -0500

    Ejercicio 2 'Routers'

---
+++ Unidad_9/Ejercicio_2/routers/animales.js
@@ -0,0 +1,55 @@
+let infoAnimales = {
+  aereos: [
+    {
+      id: 1,
+      nombre: "Águila",
+      descripcion: "Ave rapaz de gran tamaño y vuelo poderoso.",
+    },
+    {
+      id: 2,
+      nombre: "Colibrí",
+      descripcion: "Pequeño pájaro de colores brillantes y vuelo rápido.",
+    },
+    {
+      id: 3,
+      nombre: "Búho",
+      descripcion: "Ave nocturna de hábitos cazadores y vuelo silencioso.",
+    },
+  ],
+  terrestres: [
+    {
+      id: 1,
+      nombre: "León",
+      descripcion: "Felino majestuoso y poderoso, rey de la selva.",
+    },
+    {
+      id: 2,
+      nombre: "Elefante",
+      descripcion: "Mamífero de gran tamaño con trompa y colmillos.",
+    },
+    {
+      id: 3,
+      nombre: "Cebra",
+      descripcion: "Mamífero herbívoro con rayas blancas y negras.",
+    },
+  ],
+  acuaticos: [
+    {
+      id: 1,
+      nombre: "Delfín",
+      descripcion: "Cetáceo inteligente y sociable que habita en los océanos.",
+    },
+    {
+      id: 2,
+      nombre: "Tiburón",
+      descripcion:
+        "Pez depredador con cuerpo hidrodinámico y dientes afilados.",
+    },
+    {
+      id: 3,
+      nombre: "Tortuga marina",
+      descripcion: "Reptil marino con caparazón que vive en los océanos.",
+    },
+  ],
+};
+module.exports.infoAnimales = infoAnimales; //exportacion de los animales
