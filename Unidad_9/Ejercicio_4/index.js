5ab900e202bfee6cd399de8467eb4777ac906a63
nortigozab <77377179+nortigozab@users.noreply.github.com>
2023-06-24 00:38:57 -0500

    Ejercicio 4 'Ejercicio Basico'

---
+++ Unidad_9/Ejercicio_4/index.js
@@ -0,0 +1,32 @@
+require("dotenv").config();
+
+const cors = require("cors");
+const express = require("express");
+const mongoose = require("mongoose");
+const mongoString = process.env.DATABASE_URL;
+const port = process.env.PORT;
+const host = process.env.HOST;
+
+mongoose.connect(mongoString);
+const database = mongoose.connection;
+
+database.on("error", (error) => {
+  console.log(error);
+});
+
+database.once("connected", () => {
+  console.log("Database Connected");
+});
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const routes = require("./routes/routes");
+
+app.use("/api", routes);
+
+app.listen(port, () => {
+  console.log(`Server started at ${port}`);
+  console.log(`Go to http://${host}:${port}`);
+});
